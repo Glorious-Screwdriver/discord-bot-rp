00da9e4f9980dabd931beb0b6868879e62213038
Viacheslav Tsvetkov <clif2501@gmail.com>
2021-04-29 01:58:39 +0300

    Discord API, project initialisation

---
+++ src/main/java/glorious/screwdriver/Main.java
@@ -0,0 +1,13 @@
+package glorious.screwdriver;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String token = "ODM0Nzk0OTEwMzA5NDE3MDAw.YIGFWA.MtuH8-N9XmUIfEQP9FKIzuxi5nc";
+
+    }
+
+}
